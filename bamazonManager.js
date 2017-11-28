07eff7de7aa39249018461a69bf839e428a19534
unknown <benjah88@gmail.com>
2017-11-27 19:09:02 -0800

    bonus1 done

--- bamazonManager.js
+++ bamazonManager.js
@@ -1,5 +1,7 @@
 var inquirer = require("inquirer");
 var mysql = require('mysql');
+var lowflag = false;
+var total;
 var con = mysql.createConnection({
 	host: "localhost",
 	user: "Ben",
@@ -8,10 +10,59 @@ var con = mysql.createConnection({
 
 });
 
+start();
 
-display();
+function start() {
 
-function display() {
+	con.query("SELECT * FROM products", function(err, result, fields) {
+		if (err) throw err;
+
+		for (var i = 0; i < result.length; i++) {
+
+			total = result[i].item_id;
+		}
+	});
+
+	lowflag = false;
+
+	inquirer
+		.prompt([
+
+			{
+				type: "list",
+				message: "\nWhat would you like to do?",
+				choices: ["view products for sale", "view low inventory",
+					"add to inventory", "add new product"
+				],
+				name: "options",
+
+
+			}
+		]).then(function(input) {
+
+			switch (input.options) {
+
+				case 'view products for sale':
+					viewProds();
+					break;
+
+				case 'view low inventory':
+					viewLow();
+					break;
+
+				case 'add to inventory':
+					addInv();
+					break;
+
+				case 'add new product':
+					addProd();
+					break;
+			}
+		})
+}
+
+
+function viewProds() {
 
 	con.query("SELECT * FROM products", function(err, result, fields) {
 		if (err) throw err;
@@ -28,22 +79,58 @@ function display() {
 
 	setTimeout(function() {
 
-		buy();
+		start();
 
 	}, 500);
 }
 
-function buy() {
+function viewLow() {
+
+	con.query("SELECT * FROM products", function(err, result, fields) {
+		if (err) throw err;
+
+		for (var i = 0; i < result.length; i++) {
+
+			if (result[i].stock_quant < 6) {
+
+				lowflag = true;
+				console.log("\nProduct id: " + result[i].item_id);
+				console.log("Product name: " + result[i].product_name);
+				console.log("Department name: " + result[i].dept_name);
+				console.log("Product price: " + result[i].price);
+				console.log("Stock quantity: " + result[i].stock_quant);
+			}
+
+		}
+
+		if (lowflag === false) {
+
+			console.log("\nNo low inventory!!");
+			start();
+		}
+
+	});
+
+	if (lowflag === true) {
+		setTimeout(function() {
+
+			start();
+		}, 500);
+
+	}
+}
+
+function addInv() {
 
 	inquirer
 		.prompt([
 
 			{
 				type: "input",
-				message: "Type in the Id of the product you would like to buy:",
+				message: "Type in the Id of the product you would like to add:",
 				name: "prod",
 				validate: function(num) {
-					if (isNaN(num) || num > 10) {
+					if (isNaN(num) || num > total || num < 1) {
 						return false;
 					} 
 
@@ -53,10 +140,10 @@ function buy() {
 				}
 			}, {
 				type: "input",
-				message: "Type in the amount of units you would like to buy:",
+				message: "Type in the amount of units you would like to add:",
 				name: "amount",
 				validate: function(num) {
-					if (isNaN(num)) {
+					if (isNaN(num) || num < 1) {
 						return false;
 					} 
 
@@ -72,53 +159,70 @@ function buy() {
 			con.query("SELECT * FROM products where item_id =" + data.prod + "",
 				function(err, result, fields) {
 					if (err) throw err;
-					if (JSON.parse(data.amount) <= result[0].stock_quant) {
-						console.log("\nOrder placed! Total amount is: $" + data.amount * result[0].price);
+					var sql = "UPDATE products SET stock_quant = '" +
+						(result[0].stock_quant + JSON.parse(data.amount)) +
+						"' WHERE stock_quant = '" + result[0].stock_quant + "'";
 
-						var sql = "UPDATE products SET stock_quant = '" +
-							(result[0].stock_quant - JSON.parse(data.amount)) +
-							"' WHERE stock_quant = '" + result[0].stock_quant + "'";
+					con.query(sql, function(err, result) {
+						if (err) throw err;
+					});
+					console.log("\ninventory updated!");
+					start();
+				})
+		});
+}
 
-						con.query(sql, function(err, result) {
-							if (err) throw err;
-						});
+function addProd() {
 
-						console.log("\ninventory updated!");
-						agains();
+	inquirer
+		.prompt([
 
+			{
+				type: "input",
+				message: "Which product would like to add:",
+				name: "prod",
+
+			}, {
+				type: "input",
+				message: "Which department is the product in?",
+				name: "dept",
+
+			}, {
+				type: "input",
+				message: "Type in the price of the product:",
+				name: "price",
+				validate: function(num) {
+					if (isNaN(num) || num < 1) {
+						return false;
 					} 
 
 					else {
-
-						console.log("\nNot enough inventory!\n");
-						buy();
+						return true;
 					}
-				})
-			});
-}
+				}
 
-function agains() {
-	inquirer.prompt([{
-		type: "confirm",
-		message: "Would you like to see the products again?",
-		name: "conf",
-		default: true
+			}, {
+				type: "input",
+				message: "Type in the amount of units you would like to add:",
+				name: "amount",
+				validate: function(num) {
+					if (isNaN(num) || num < 1) {
+						return false;
+					} 
 
-	}]).then(function(again) {
+					else {
+						return true;
+					}
+				}
 
-		if (again.conf) {
+			}
+		])
+		.then(function(data) {
 
-			display();
+			con.query("INSERT INTO products (product_name, dept_name, price, stock_quant) values('" + data.prod + "', '" + data.dept + "'," + data.price + "," + data.amount + ")");
 
-		} 
-
-		else {
-
-			console.log("\nBYE!!");
-			con.end();
-
-		}
-
-	})
+			console.log("\nPoduct added!");
+			start();
+		})
 
 }
